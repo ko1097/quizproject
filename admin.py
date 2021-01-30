4901da465a2662c2afa779c46c584adc67c7b1c3
ko1097 <67546597+ko1097@users.noreply.github.com>
2021-01-30 11:05:14 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Mathsquestion,Socialquestion
-
-admin.site.register(Mathsquestion)
-admin.site.register(Socialquestion)
-
-# Register your models here.

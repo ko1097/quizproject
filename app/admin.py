951d7fdfa7d71c626ef424d8eee841c080a22b51
ko1097 <67546597+ko1097@users.noreply.github.com>
2021-01-30 10:54:52 +0530

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Mathsquestion,Socialquestion
+
+admin.site.register(Mathsquestion)
+admin.site.register(Socialquestion)
+
+# Register your models here.
